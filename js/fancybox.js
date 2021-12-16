37a5346be4c4f09214d91fc2c452736d566cbfce
shomy <shomyliu@gmail.com>
2021-12-16 23:54:39 +0800

    Site updated: 2021-12-16 23:54:38

---
+++ js/fancybox.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    wrapImageWithFancyBox();
+});
+
+/**
+ * Wrap images with fancybox support.
+ */
+function wrapImageWithFancyBox() {
+    $('img').not('.sidebar-image img').not('#author-avatar img').not(".mdl-menu img").each(function() {
+
+        var $image = $(this);
+        var imageCaption = $image.attr('alt');
+        var $imageWrapLink = $image.parent('a');
+        if ($imageWrapLink.size() < 1) {
+            $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
+        }
+        $imageWrapLink.attr('data-fancybox', 'images');
+        if (imageCaption) {
+            $imageWrapLink.attr('data-caption', imageCaption);
+        }
+    });
+    $().fancybox({
+        selector : '[data-fancybox="images"]',
+        thumbs : true,
+        hash : false,
+        loop : false,
+    });
+
+}
